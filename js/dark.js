a6dca77134ca6aa006860aec5b9e1380a15a2714
Juan Gabriel <bloodbugsgamess@gmail.com>
2021-10-29 13:53:28 -0300

    My website 😎

---
+++ js/dark.js
@@ -0,0 +1,2 @@
+$(document).ready(function(){$("#toggle").click(function(){var dark=Cookies.get("dark");var domain=window.location.origin;url=domain.replace(/(https?:\/\/)?(www.)?/i,'');url=url.split('.');url=url.slice(url.length-2).join('.');if(dark){$("link[href='/css/theme-dark.css']").remove();Cookies.remove("dark",{domain:`.${url}`,sameSite:'strict',secure:true});}else{$('head').append('<link rel="stylesheet" href="/css/theme-dark.css" type="text/css" />');Cookies.set("dark",1,{expires:31,domain:`.${url}`,sameSite:'strict',secure:true});}
+return false;});});
